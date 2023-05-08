a2631eed40eb073732d77429c32b2ae51d1795f2
Mahdi Karmali <2185131@cegepmontpetit.ca>
2023-05-08 09:52:18 -0400

    FCT: configuration de la classe Baladeur.cs
    J'ai configuré les champs, les propriétés, les constructeurs et les méthodes de la classe Baladeur.cs

--- BaladeurMultiFormats/Baladeur.cs
+++ BaladeurMultiFormats/Baladeur.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,9 +27,12 @@ namespace BaladeurMultiFormats
 
         #region Constructeurs
 
+        /// <summary>
+        /// Initialise une instance de la classe Baladeur. Elle instancie la collection des chansons.
+        /// </summary>
         Baladeur()
         {
-
+            m_colChansons = new List<Chanson>();
         }
 
         #endregion
@@ -36,34 +40,137 @@ namespace BaladeurMultiFormats
 
         #region Méthodes
 
+        /// <summary>
+        /// Affiche la liste des chansons dans la pListView passée en paramètre.
+        /// </summary>
+        /// <param name="pListView"></param>
         public void AfficherLesChansons(ListView pListView)
         {
-            throw new NotImplementedException();
+            foreach (Chanson chanson in m_colChansons)
+            {
+                ListViewItem objListView = new ListViewItem(chanson.Artiste);
+                objListView.SubItems.Add(chanson.Titre);
+                objListView.SubItems.Add(chanson.Annee.ToString());
+                objListView.SubItems.Add(chanson.Format);
+
+                pListView.Items.Add(objListView);
+            }
         }
 
         public Chanson ChansonAt(int pIndex)
         {
-            throw new NotImplementedException();
+            return m_colChansons[pIndex];
         }
 
+        /// <summary>
+        /// Charge la liste des chansons dans m_colChansons. Elle doit vérifier l’existence du répertoire des chanson
+        /// Ensuite lit chaque fichier et instancie une classe de chanson selon le format et l’ajoute dans la collection des chansons m_colChansons 
+        /// </summary>
         public void ConstruireLaListeDesChansons()
         {
-            throw new NotImplementedException();
+
+            if (Directory.Exists(NOM_RÉPERTOIRE))
+            {
+
+                foreach (string fichier in Directory.GetFiles(NOM_RÉPERTOIRE))
+                {
+
+                    Chanson chanson = null;
+
+                    try
+                    {
+                        switch (fichier.Split('.')[1].ToUpper())
+                        {
+                            case "AAC":
+                                chanson = new ChansonAAC(fichier);
+                                break;
+                            case "MP3":
+                                chanson = new ChansonMP3(fichier);
+                                break;
+                            case "WMA":
+                                chanson = new ChansonWMA(fichier);
+                                break;
+                        }
+                    }
+
+                    catch (Exception e)
+                    {
+                        MessageBox.Show(e.Message);
+                    }
+
+
+                    if (chanson != null)
+                    {
+                        m_colChansons.Add(chanson);
+                    }
+                }
+
+            }
+
+
         }
 
+        /// <summary>
+        /// Instancie une ChansonAAC à partir de la chanson à l’index pIndex, enregistre les paroles et supprime le fichier du format précédent.
+        /// Elle utilise la méthode Ecrire pour enregistrer les paroles et la méthode File.Delete pour supprimer un fichier.
+        /// </summary>
+        /// <param name="pIndex"></param>
         public void ConvertirVersAAC(int pIndex)
         {
-            throw new NotImplementedException();
+            Chanson objChanson = ChansonAt(pIndex);
+
+            if (objChanson.Format == "AAC")
+            {
+                return;
+            }
+
+            ChansonAAC objChansonAAC = new ChansonAAC(NOM_RÉPERTOIRE, objChanson.Artiste, objChanson.Titre, objChanson.Annee);
+            objChansonAAC.Ecrire(objChanson.Paroles);
+
+            File.Delete(objChanson.NomFichier);
+            m_colChansons.RemoveAt(pIndex);
         }
 
+        /// <summary>
+        /// Instancie une ChansonMP3 à partir de la chanson à l’index pIndex, enregistre les paroles et supprime le fichier du format précédent.
+        /// Elle utilise la méthode Ecrire pour enregistrer les paroles et la méthode File.Delete pour supprimer un fichier.
+        /// </summary>
+        /// <param name="pIndex"></param>
         public void ConvertirVersMP3(int pIndex)
         {
-            throw new NotImplementedException();
+            Chanson objChanson = ChansonAt(pIndex);
+
+            if (objChanson.Format == "MP3")
+            {
+                return;
+            }
+
+            ChansonMP3 chansonMP3 = new ChansonMP3(NOM_RÉPERTOIRE, objChanson.Artiste, objChanson.Titre, objChanson.Annee);
+            chansonMP3.Ecrire(objChanson.Paroles);
+
+            File.Delete(objChanson.NomFichier);
+            m_colChansons.RemoveAt(pIndex);
         }
 
+        /// <summary>
+        /// Instancie une ChansonWMA à partir de la chanson à l’index pIndex, enregistre les paroles et supprime le fichier du format précédent.
+        /// Elle utilise la méthode Ecrire pour enregistrer les paroles et la méthode File.Delete pour supprimer un fichier.
+        /// </summary>
+        /// <param name="pIndex"></param>
         public void ConvertirVersWMA(int pIndex)
         {
-            throw new NotImplementedException();
+            Chanson objChanson = ChansonAt(pIndex);
+
+            if (objChanson.Format == "WMA")
+            {
+                return;
+            }
+
+            ChansonWMA chansonWMA = new ChansonWMA(NOM_RÉPERTOIRE, objChanson.Artiste, objChanson.Titre, objChanson.Annee);
+            chansonWMA.Ecrire(objChanson.Paroles);
+
+            File.Delete(objChanson.NomFichier);
+            m_colChansons.RemoveAt(pIndex);
         }
 
         #endregion
